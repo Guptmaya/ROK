d6e102f4a7d9050136e5a48fcc5c2650d3fd7ade
guptmaya <guptmaya@sheridancollege.ca>
2022-04-11 19:35:43 -0400

    Update hunt.js

--- commands/hunt.js
+++ commands/hunt.js
@@ -90,8 +90,9 @@ module.exports = {
               .catch(err => {
                 console.log(err);
               })
-              col.on('end', async () => { return;})
-           
+            col.stop();
+            return;
+
           } else if (winner === "barb") {
             change = "no";
             let attackEmbed = new Discord.MessageEmbed()
@@ -101,7 +102,8 @@ module.exports = {
               .setDescription("\`\`\`Failed to hunt the barbarian.\n" +
                 `Tip : Increase your level with bot to boost your attack and health buffs.\`\`\``)
             await curPage.edit({ embeds: [attackEmbed], components: [deadRow], files: [file] })
-            col.on('end', async () => {return;})
+            col.stop();
+            return;
           }
         }
 
@@ -113,8 +115,9 @@ module.exports = {
             .setThumbnail(barbImage)
             .setDescription("Run the \`r!hunt barb\` to find a new barbarian.")
           await curPage.edit({ embeds: [skippedEmbed], components: [deadRow], files: [file] })
-          col.on('end', async () => {return;})
-          
+          col.stop();
+          return;
+
         }
 
         await curPage.edit({ embeds: [embed], components: [row], files: [file] })

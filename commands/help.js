8260b8fc5e8161348801d9568e1a329f7b87a167
guptmaya <guptmaya@sheridancollege.ca>
2022-04-10 20:57:43 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,29 +10,30 @@ module.exports = {
   async execute(bot, message, args) {
 
     let desc = "";
-      desc = `\`\*\*\*Calculation Commands\*\*\*\n${prefix.prefix1}mge \` : Calculates MGE Points\n\n` +
-        `\`${prefix.prefix1}ap \` : Calculates Total Action Points\n\n` +
-        `\`${prefix.prefix1}tok \` : Calculates Total Tome of Knowledge\n\n` +
-        `\`${prefix.prefix1}rss \` : Calculates Total Resources\n\n` +
-        `\`${prefix.prefix1}gems \` : Calculates Total Gems\n\n` +
-        `\`${prefix.prefix1}goldchest [number of keys]\` : Simulates Gold Chest unlock\n\n` +
-        `\`${prefix.prefix1}wheel \` : Simulates spinning wheel of fortune\n\n`+
-        `\`Game Commands`+
-        `\`${prefix.prefix1}register \` : To register yourself with bot\n\n`+
-        `\`${prefix.prefix1}profile \` : To check your profile\n\n`+
-        `\`${prefix.prefix1}tutorial \` : To read tutorial\n\n`+
-        `\`${prefix.prefix1}hunt barb\` : To hunt barbarians\n\n`;
-   
+    desc = `\*\*\*Calculation Commands\*\*\*\n` +
+      `\`${prefix.prefix1}mge \` : Calculates MGE Points\n\n` +
+      `\`${prefix.prefix1}ap \` : Calculates Total Action Points\n\n` +
+      `\`${prefix.prefix1}tok \` : Calculates Total Tome of Knowledge\n\n` +
+      `\`${prefix.prefix1}rss \` : Calculates Total Resources\n\n` +
+      `\`${prefix.prefix1}gems \` : Calculates Total Gems\n\n` +
+      `\`${prefix.prefix1}goldchest [number of keys]\` : Simulates Gold Chest unlock\n\n` +
+      `\`${prefix.prefix1}wheel \` : Simulates spinning wheel of fortune\n\n` +
+      `\*\*\*Game Commands\*\*\*` +
+      `\`${prefix.prefix1}register \` : To register yourself with bot\n\n` +
+      `\`${prefix.prefix1}profile \` : To check your profile\n\n` +
+      `\`${prefix.prefix1}tutorial \` : To read tutorial\n\n` +
+      `\`${prefix.prefix1}hunt barb\` : To hunt barbarians\n\n`;
 
 
-      let footer = `${bot.user.username} | Uta#3963`;
-      let embed = new Discord.MessageEmbed()
-        .setTitle("[ HELP ]")
-        .setColor("GREEN")
-        .setThumbnail(bot.user.displayAvatarURL())
-        .setDescription(desc)
-        .setFooter(footer)
-      message.channel.send({ embeds: [embed] });
-    
+
+    let footer = `${bot.user.username} | Uta#3963`;
+    let embed = new Discord.MessageEmbed()
+      .setTitle("[ HELP ]")
+      .setColor("GREEN")
+      .setThumbnail(bot.user.displayAvatarURL())
+      .setDescription(desc)
+      .setFooter(footer)
+    message.channel.send({ embeds: [embed] });
+
   },
 };
